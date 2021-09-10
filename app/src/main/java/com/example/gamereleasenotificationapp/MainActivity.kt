587e5d5854065ca9780d3bbd46fa1aa18b303006
matchimachi <matis7142@icloud.com>
2021-09-10 23:30:04 +0900

    [fix]企業一覧画面のListview作成

--- app/src/main/java/com/example/gamereleasenotificationapp/MainActivity.kt
+++ app/src/main/java/com/example/gamereleasenotificationapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.gamereleasenotificationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 配列の生成
+        val array = arrayOf((R.string.company_name1).toString(), (R.string.company_name2).toString(), (R.string.company_name3).toString(), (R.string.company_name4).toString(), (R.string.company_name5).toString())
+
+        // xmlにて実装したListViewの取得
+        val listView = findViewById<ListView>(R.id.list_view)
+
+        // ArrayAdapterの生成
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array)
+
+        // ListViewに、生成したAdapterを設定
+        listView.adapter = adapter
     }
 }
