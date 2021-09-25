41ebd187ec930afb11a072f8e634bbb98c342ab0
matchimachi <matis7142@icloud.com>
2021-09-25 23:54:28 +0900

    [fix]企業一覧のリストの処理をMainActivityからfragmentへ移行

--- app/src/main/java/com/example/gamereleasenotificationapp/GameListFragment.kt
+++ app/src/main/java/com/example/gamereleasenotificationapp/GameListFragment.kt
@@ -1,18 +1,15 @@
 package com.example.gamereleasenotificationapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.gamereleasenotificationapp.placeholder.PlaceholderContent
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
-/**
- * A fragment representing a list of Items.
- */
+
 class GameListFragment : Fragment() {
 
     private var columnCount = 2
@@ -25,22 +22,38 @@ class GameListFragment : Fragment() {
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // 配列の生成
+        val array = arrayOf(
+            getString(R.string.company_name1),
+            getString(R.string.company_name2),
+            getString(R.string.company_name3),
+            getString(R.string.company_name4),
+            getString(R.string.company_name5)
+        )
+
+        // xmlにて実装したListViewの取得
+        val listView = view?.findViewById<ListView>(R.id.list_view)
+
+        // ArrayAdapterの生成
+        val adapter = this.getContext()
+            ?.let { ArrayAdapter<String>(it, android.R.layout.simple_list_item_1, array) }
+
+        // ListViewに、生成したAdapterを設定
+        listView?.adapter = adapter
+
+        // 項目をタップしたときの処理
+        listView?.setOnItemClickListener { parent, view, position, id ->
+            // 色
+            Log.i("NewItemFragment", "わあああああ")
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_game_list_view, container, false)
-
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MyItemRecyclerViewAdapter(PlaceholderContent.ITEMS)
-            }
-        }
         return view
     }
 
