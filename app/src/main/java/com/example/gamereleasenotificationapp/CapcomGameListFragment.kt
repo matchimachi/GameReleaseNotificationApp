6b03c7e5f13b56c264c73d7b81918066c7046460
matchimachi <matis7142@icloud.com>
2021-10-17 22:14:23 +0900

    [try]Kotlin fun showPopup(v

--- app/src/main/java/com/example/gamereleasenotificationapp/CapcomGameListFragment.kt
+++ app/src/main/java/com/example/gamereleasenotificationapp/CapcomGameListFragment.kt
@@ -1,21 +1,56 @@
 package com.example.gamereleasenotificationapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
+import android.widget.PopupMenu
 
 
 class CapcomGameListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val btnGetCurrentBit: Button = view.findViewById(R.id.btnGetCurrentBit)
+        btnGetCurrentBit.setOnClickListener{
+            fun showPopup(v: View) {
+                val popup = PopupMenu(context!!, v)
+                val inflater = popup!!.menuInflater
+                inflater.inflate(R.menu.header_menu, popup!!.menu)
+                popup!!.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                    override fun onMenuItemClick(item: MenuItem?): Boolean {
+                        when (item!!.itemId) {
+                            R.id.menu_open_in_browser -> {
+                                //code here
+                                return true
+                            }
+                            R.id.menu_cancel -> {
+                                popup!!.dismiss()
+                                //code here
+                                return true
+                            }
+                            else -> return false
+                        }
+                    }
+                })
+
+//            popup!!.menu.findItem(R.id.menu_open_in_browser).applyFont(v.context, R.font.lato)
+
+
+                popup!!.show()
+            }
+            showPopup(view)
+        }
+
         // 配列の生成
         val array = arrayOf(
             getString(R.string.game_name1),
@@ -32,6 +67,7 @@ class CapcomGameListFragment : Fragment() {
         // ListViewに、生成したAdapterを設定
         listView?.adapter = adapter
 
+
         // 項目をタップしたときの処理
 //        listView?.setOnItemClickListener { parent, view, position, id ->
 //            Log.i("NewItemFragment", "ゲーム一覧へ遷移")
@@ -44,10 +80,15 @@ class CapcomGameListFragment : Fragment() {
     }
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_capcom_game_list_view, container, false)
         return view
+
     }
+
+
+
 }
